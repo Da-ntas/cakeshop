0bbd4d6c393893b9a4b1927884302a0ac57c7c1a
Hugo Fernandes <hugodfl2002@gmail.com>
2025-01-17 23:31:09 -0300

    - DB - Creation of order and status table

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, serial, text, boolean, integer, timestamp } from "drizzle-orm/pg-core";
+import { pgTable, serial, text, boolean, integer, numeric, varchar, timestamp } from "drizzle-orm/pg-core";
 
 export const user = pgTable('user', {
     codUser: serial('codUser').primaryKey(),
@@ -8,5 +8,27 @@ export const user = pgTable('user', {
     dscRoles: text("dscRoles").notNull().default("user"),
     flagInactive: boolean("flagInactive").notNull().default(false),
     dtaCrated: timestamp('dtaCrated', { withTimezone: true }).notNull().defaultNow(),
+    dtaUpdated: timestamp('dtaUpdated', { withTimezone: true })
+})
+
+export const status = pgTable('status', {
+    codStatus: varchar('codStatus', { length: 8 }).primaryKey(),
+    nomStatus: varchar("nomStatus", {length: 25}).notNull().unique(),
+    nomStatusMaster: varchar("nomStatusMaster", {length: 25}).notNull(),
+    createdBy: integer('createdBy').references(() => user.codUser),
+    dtaCrated: timestamp('dtaCrated', { withTimezone: true }).notNull().defaultNow(),
+    updatedBy: integer('updatedBy').references(() => user.codUser),
+    dtaUpdated: timestamp('dtaUpdated', { withTimezone: true })
+})
+
+export const order = pgTable('order', {
+    codOrder: serial('codOrder').primaryKey(),
+    codUser: integer('codUser').references(() => user.codUser),
+    codStatus: varchar('codStatus', {length: 8}).references(() => status.codStatus),
+    vlrTotalOrder: numeric('vlrTotalOrder', {precision: 15, scale: 3}).notNull(),
+    dtaToDeliver: timestamp('dtaToDeliver', {withTimezone: true}).notNull(),
+    createdBy: integer('createdBy').references(() => user.codUser),
+    dtaCrated: timestamp('dtaCrated', { withTimezone: true }).notNull().defaultNow(),
+    updatedBy: integer('updatedBy').references(() => user.codUser),
     dtaUpdated: timestamp('dtaUpdated', { withTimezone: true })
 })

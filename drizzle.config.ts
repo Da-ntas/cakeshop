9515553167c65969dc02d41b20bfb731f3305c2a
Hugo Fernandes <hugodfl2002@gmail.com>
2025-01-25 00:32:47 -0300

    - Drizzle.config personalization to auto add views and tables on schema prop

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'drizzle-kit'
 import { env } from './src/env'
+import { existsSync, readdirSync } from 'node:fs';
+
+const extra: Array<string> = [];
+
+const viewsPath = "./src/db/views";
+if (existsSync(viewsPath)) {
+  const views = readdirSync(viewsPath)
+    .filter((file) => file.endsWith('.ts'))
+    .map((file) => `${viewsPath}/${file}`);
+
+  extra.push(...views);
+}
+
+const tablesPath = "./src/db/tables";
+if (existsSync(tablesPath)) {
+  const views = readdirSync(tablesPath)
+    .filter((file) => file.endsWith('.ts'))
+    .map((file) => `${tablesPath}/${file}`);
+
+  extra.push(...views);
+}
+
 export default defineConfig({
-  schema: "./src/db/schema.ts",
+  schema: ["./src/db/schema.ts", ...extra],
   out: './.migrations',
   dialect: 'postgresql',
   dbCredentials: {
